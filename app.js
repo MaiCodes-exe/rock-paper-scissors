6c87b63506aa35f74a61d4eb7c8e86ee3e1f6bed
MaiCodes-exe <maise.b.costa@gmail.com>
2023-03-29 18:24:43 +0100

    adeed results logic

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const resultDisplay = document.getElementById("results")
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result 
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) =>{
 userChoice = e.target.id
 userChoiceDisplay.innerHTML = userChoice
 generateComputerChoice()
+getResult()
 }))
 
 function generateComputerChoice(){
@@ -24,4 +26,31 @@ if(randomNumber == 3){
     computerChoice = 'scissors'
 }
 computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult(){
+    if (computerChoice === userChoice){
+        result = 'It is a draw'
+    }
+    if (computerChoice === userChoice){
+        result = 'It is a draw'
+    }
+    if (computerChoice === 'rock' &&  userChoice == 'paper'){
+        result = 'You win!'
+    }
+    if (computerChoice === 'rock' &&  userChoice == 'scissors'){
+        result = 'You win!'
+    }
+    if (computerChoice === 'paper' &&  userChoice == 'scissors'){
+        result = 'You win!'
+    }
+
+    if (computerChoice === 'paper' &&  userChoice == 'rock'){
+        result = 'You lose!'
+    }
+    
+    if (computerChoice === 'scissors' &&  userChoice == 'paper'){
+        result = 'You lose!'
+    }
+resultDisplay.innerHTML = result
 }
